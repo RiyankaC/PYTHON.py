30b167808eee7ffab41a42bf6ad44039f859c40a
RiyankaC <riyankachakraborty43@gmail.com>
2025-05-04 07:04:54 +0000

    areaofshapes.py

---
+++ area of shapes.py
@@ -0,0 +1,23 @@
+#AREA OF SHAPES
+'''
+  AREA OF A TRIANGLE
+'''
+
+#formula=0.5*base*height
+base=float(input("base of triangle is :"))
+height=float(input("height of triangle is :"))
+area=0.5*base*height
+print(area)
+
+
+'''
+AREA OF RECTANGLE
+'''
+#FORMULA = LENGTH * BREADTH * HEIGHT
+length=float(input("enter length of rectangle:"))
+breadth=float(input("enter breadth of rectangle :"))
+height=float(input("enter height of rectangle:"))
+
+area=length*breadth*height
+print("AREA OF THE RECTANGLE IS :",area)
+
